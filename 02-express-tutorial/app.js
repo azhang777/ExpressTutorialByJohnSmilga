4625dd79e62e3770a38fc0332089c1fe34205cbd
Andy Zhang <67079259+azhang777@users.noreply.github.com>
2023-02-28 00:18:46 -0500

    middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,45 +1,23 @@
 const express = require("express");
-const { products } = require("./data");
 const app = express();
-/*
-app.get("/", (req, res) => {
-  res.json(products);
-});
-*/
+const logger = require("./logger");
+const authorize = require("./authorize");
+app.use("/", [logger, authorize]);
 
 app.get("/", (req, res) => {
-  res.send("<h1> Home Page </h1> <a href='/api/products'> products </a>");
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("about");
 });
 
 app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
+  res.send("Products");
 });
 
-app.get("/api/products/1", (req, res) => {
-  const singleProduct = products.find((product) => product.id === 1);
-  res.json(singleProduct);
-});
-
-app.get("/api/products/:productID", (req, res) => {
-  //console.log(req);
-  //console.log(req.params);
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    res.status(404).send("Product does not exist!");
-  }
-  res.json(singleProduct);
-});
-
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("hello world");
+app.get("/api/items", (req, res) => {
+  res.send("items");
 });
 app.listen(6313, () => {
   console.log("app is listening to port 6313");
