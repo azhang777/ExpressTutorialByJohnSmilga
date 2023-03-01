dd690a01cf152f0bfd00332b4cde370f762d36aa
Andy Zhang <67079259+azhang777@users.noreply.github.com>
2023-03-01 00:00:49 -0500

    post

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,24 +1,13 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
-const authorize = require("./authorize");
-app.use("/", [logger, authorize]);
+let { people } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send("Home");
+//static assets
+app.use(express.static("./methods-public"));
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get("/about", (req, res) => {
-  res.send("about");
-});
-
-app.get("/api/products", (req, res) => {
-  res.send("Products");
-});
-
-app.get("/api/items", (req, res) => {
-  res.send("items");
-});
 app.listen(6313, () => {
   console.log("app is listening to port 6313");
 });
