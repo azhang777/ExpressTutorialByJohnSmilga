65368da7da72b39746029ace1757ef03f7411328
Andy Zhang <67079259+azhang777@users.noreply.github.com>
2023-03-04 21:30:42 -0500

    post

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,10 +4,39 @@ let { people } = require("./data");
 
 //static assets
 app.use(express.static("./methods-public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).send({ success: true, person: name });
+});
+
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).send({ success: true, data: [...people, name] });
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please provide credentials");
+});
 app.listen(6313, () => {
   console.log("app is listening to port 6313");
 });
